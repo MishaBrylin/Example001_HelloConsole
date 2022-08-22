60b58842b54ba7bf43f36e909ad5cd8f2433dc2c
Misha <79090171508@yandex.ru>
2022-08-22 20:54:25 +0500

    ifelse

--- Example005_Condition/Program.cs
+++ Example005_Condition/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура Это Маша");
+
+}
+
+else 
+{
+    Console.Write("Привеn, ");
+    Console.WriteLine(username);
+}
